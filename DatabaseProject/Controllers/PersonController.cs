60d404cadb697c92a63fc7074aa88c899b9ae3d8
captainkrunch <bentleyp@goldmail.etsu.edu>
2016-04-13 14:01:14 -0400

    update (reverted from commit 7ab05e141ee682976fa210e13d9e1d77537cae98)
    Zac Slack 4/11

---
+++ DatabaseProject/Controllers/PersonController.cs
@@ -0,0 +1,104 @@
+﻿using DatabaseProject.DataContexts;
+using DatabaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseProject.Controllers
+{
+    public class PersonController : Controller
+    {
+        private DataDb ddb = new DataDb();
+
+        protected override void Dispose(bool disposing)
+        {
+            ddb.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Index()
+        {
+            return View(ddb.People.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                ddb.People.Add(person);
+                ddb.SaveChanges();
+
+                return RedirectToAction("Index","Person");
+            }
+            return View(person);
+        }
+
+        public ActionResult Details(int id = 0)
+        {
+            Person person = ddb.People.Find(id);
+
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Person person = ddb.People.Find(id);
+
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                ddb.Entry(person).State = System.Data.Entity.EntityState.Modified;
+                ddb.SaveChanges();
+                return RedirectToAction("Index", "Person");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id = 0)
+        {
+            Person person = ddb.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePerson(int id = 0)
+        {
+            Person person = ddb.People.Find(id);
+
+            if (person != null)
+            {
+                ddb.People.Remove(person);
+                ddb.SaveChanges();
+                return RedirectToAction("Index", "Person");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ DatabaseProject/DataContexts/DataDb.cs
@@ -0,0 +1,33 @@
+﻿using DatabaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseProject.DataContexts
+{
+    public class DataDb : DbContext
+    {
+        public DataDb() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Class> Classes { get; set; }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Person_Class> P_Classes { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Person_Team> P_Teams { get; set; }
+
+        public DbSet<Person_Event> P_Events { get; set; }
+    }
+
+}
